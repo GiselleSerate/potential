94d0e09f76d402ca17ba51fc6f6a3dc4e27e53bb
NixAzrael <gserate@davidsonacademy.unr.edu>
2015-07-29 10:39:43 -0700

    Pound sign removed when creating stage--made things draw and show up and stuff again. Wow. Also the actual drag block is no longer there, so that's something I have to fix.

--- codeBlocks.js
+++ codeBlocks.js
@@ -11,11 +11,16 @@ function init() {
 
 	var whichTime = 0;
 
-	// var canvas = document.getElementById("demoCanvas");
-	var stage = new createjs.Stage("#demoCanvas");
+	var stage = new createjs.Stage("demoCanvas");
 
-	var codeComponents = [];
-	var codeTypes = [];
+	//Constructor function for code pieces.
+	function CodePiece(piece, kind) {
+		this.piece = piece;
+		this.kind = kind;
+	}
+
+	//Creates array for storing components that are created.
+	var codeList = [];
 
 	// Initializing dimensions of spawn and drop space.
 	var topSpawn = 0;
@@ -25,29 +30,33 @@ function init() {
 	var leftSide = $('#demoCanvas').width()/2;
 	console.log(topSpawn + " " + topDrop + " " + bottomDrop + " " + leftSide + " " + rightSide); 
 
-	var xPointList = [leftSide]; 
-	var yPointList = [topDrop];
+	//Creates point constructor function. 
+	function Point(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	//Creates array for storing possible points to snap to. 
+	var pointList = [new Point(leftSide, topDrop)];
+
 	var neighbourX;
 	var neighbourY;
 
 	var dropSite = new createjs.Shape();
-	dropSite.graphics.beginFill("black").drawRect(0,0,200,40);
+	dropSite.graphics.beginFill("red").drawRect(0,0,200,40);
 	dropSite.x = leftSide;
-	dropSite.y = topDrop;
+	dropSite.y = topDrop + 40;
 	stage.addChild(dropSite);
 	stage.update();
 
 	var createRepeatBlock = function() {
-		codeComponents.push(new createjs.Shape());
-		var last = codeComponents.length-1
-		codeComponents[last].graphics.beginFill("DeepSkyBlue").drawRect(0,0,200,40);
-		codeComponents[last].x = leftSide;
-		codeComponents[last].y = topSpawn;
-		stage.addChild(codeComponents[last]);
+		codeList.push(new createjs.Shape(), "repeat");
+		var last = 0;
+		codeList[last].piece.graphics.beginFill("DeepSkyBlue").drawRect(0,40,200,40);
+		codeList[last].piece.x = leftSide;
+		codeList[last].piece.y = topSpawn;
+		stage.addChild(codeList[last].piece);
 		stage.update();
-		codeTypes.push("repeatBlock");
-		console.log(codeComponents);
-		console.log(codeTypes);
 	}
 
 	createRepeatBlock();
@@ -86,38 +95,30 @@ function init() {
 	};
 
 
-	for(var i=0;i< codeComponents.length; i++) {
+	for(var i=0;i< codeList.length; i++) {
 		//Calls pleaseMove function when mouse is clicked.
-	    codeComponents[i].on("pressmove", function(evt) {
+	    codeList[i].piece.on("pressmove", function(evt) {
 			pleaseMove(i, evt);
 		});
 
 	    //Calls pleaseDrop function when mouse is no longer clicked.
-		codeComponents[i].on("pressup", function(evt) { 
+		codeList[i].piece.on("pressup", function(evt) { 
 			pleaseDrop(i, evt);
 			whichTime = 0;
 		})
 	}
 
-	// codeComponents[0].on("pressmove", function(evt) {
-	// 	pleaseMove(0, evt);
-	// });
-
-	// codeComponents[0].on("pressup", function(evt) { 
-	// 	pleaseDrop(0, evt);
-	// })
-
 	// Snaps block to anything that exists in the working space
 	var snapTo = function(index) {
-		var block = codeComponents[index];
+		var block = codeList[index].piece;
 
 		// console.log(block);
 		// var originalX = block.x;
 		// var originalY = block.y;
 		for(var num = 0; num < xPointList.length; num++) {
 			// Determine the distance from the mouse position to the point
-			var diffX = Math.abs(MouseEvent.stageX - xPointList[num]);
-			var diffY = Math.abs(MouseEvent.stageY - yPointList[num]); 
+			var diffX = Math.abs(MouseEvent.stageX - pointList[num].x);
+			var diffY = Math.abs(MouseEvent.stageY - pointList[num].y); 
 			var d = Math.sqrt(diffX*diffX + diffY*diffY);        
 
 			// If the current point is closeEnough and the closest (so far)
@@ -125,14 +126,14 @@ function init() {
 			var snapDistance = 100;
 			var closest = (d<snapDistance && (dist == null || d < dist));
 			if (closest) {
-				neighbourX = xPointList[num];
-				console.log(xPointList);
-				neighbourY = yPointList[num];
+				neighbourX = pointList[num].x;
+				console.log(pointList[num].x);
+				neighbourY = pointList[num].y;
 			}
 			//Else if it's the first time through in a new drag sequence, set neighbourX and neighbourY to the block's initial coordinates.
 			else if (whichTime === 0) {
-				neighbourX = xPointList[0];
-				neighbourY = yPointList[0];
+				neighbourX = pointList[0].x;
+				neighbourY = pointList[0].y;
 			}          
 			//Else, leave neighbourX and neighbourY as whatever they were the last time you ran snapTo.
 		}
