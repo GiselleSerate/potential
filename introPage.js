99eba8632270a76bf6fb2ff261b63ea2b90720bc
syasko <syasko.sj@mysummitps.org>
2015-07-29 10:41:44 -0700

    Applied background also made the start button and title disappear when start button is pressed

--- introPage.js
+++ introPage.js
@@ -1,12 +1,15 @@
+var start;
+var logo;
 $(document).ready(function () {
 
-    $("#startButton").append('<img src = "StartButton.png" onclick="nextPage()">');
-     $("#Logo").append('<p id= "title"> Potential</p><p id= "subtitle"> An interactive story adventure!</p>');
+    start=$("#startButton").append('<img src = "StartButton.png" onclick="nextPage()">');
+     logo=$("#Logo").append('<p id= "title"> Potential</p><p id= "subtitle"> An interactive story adventure!</p>');
 });
 
 
 
 var nextPage = function(){
-	console.log("hi")
+	start.remove();
+	logo.remove();
 
 }
