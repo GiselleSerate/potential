31b17637cf35d700a7c14ebd99db999fb709d50b
jeanellet <jtanhueco.ra@mysummitps.org>
2015-08-04 09:30:26 -0700

    animation to lvl 1

--- codeBlocks.js
+++ codeBlocks.js
@@ -335,6 +335,74 @@ function init() {
 			return false;
 		}
 	}
+
+
+
+
+        	//create variables you need
+        	var stage1, instance2;
+
+            //initialize stage variable to your html canvas element
+            stage1 = new createjs.Stage("demoCanvas3");
+
+            //data object that holds animation info-spritesheet and how to break it up
+            var data = {
+                images: ["monstersheetlvl1.png"],
+                //this says each individual sprite is 180 wide and 247.5 tall (view image size in photoshop and do the math to get these numbers)
+                frames: {width:80, height:177, count:3},
+                //define animations here by giving frame indexes (remember to start at zero)
+                animations: {
+                    stand:0,
+                    run:[0,2]
+                }
+            };
+            var data2 = {
+                images: ["girlsheetlvl1.png"],
+                //this says each individual sprite is 180 wide and 247.5 tall (view image size in photoshop and do the math to get these numbers)
+                frames: {width:72, height:128, count:3},
+                //define animations here by giving frame indexes (remember to start at zero)
+                animations: {
+                    stand:0,
+                    run:[0,2]
+                }
+            };
+
+            //these two lines pair together and are both necessary:
+            //create sprite sheet from the data
+            var ss = new createjs.SpriteSheet(data);
+            var ss2 = new createjs.SpriteSheet(data2);
+            //create animation from the sprite sheet
+            var instance = new createjs.Sprite(ss);
+            instance2 = new createjs.Sprite(ss2);
+
+            //give your animation starting coordinates: (0,0) is top left
+            instance.x = 550;
+            instance.y = 400;
+            //instance2.x = 0;
+            instance2.y = 450;
+
+            //need to add instance to stage (everything needs to be added to the stage)
+            stage1.addChild(instance);
+            stage1.addChild(instance2);
+            //this starts the animation and repeats forever
+            //gotoAndStop would cause it to run once
+            //can also specify a animation to follow, look at docs
+            instance.gotoAndPlay("run");
+            instance2.gotoAndPlay("run");
+
+
+            //these two lines continully update the stage
+            createjs.Ticker.setFPS(6);
+            createjs.Ticker.addEventListener("tick", tick);
+          	function tick(event) {
+	            instance2.x = instance2.x + 10;
+	            if (instance2.x > 250) { 
+	                instance2.gotoAndPlay("stand"); 
+	                return false;
+	            }
+            	stage1.update(event); // important!!
+        	}
+
 }
 
 //Handles what happens if you win. 
